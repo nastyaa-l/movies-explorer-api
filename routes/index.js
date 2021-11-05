fe8a56a197cc8b881760f7f7e7e726a7312da7ca
Anastasia Lyugaeva <nastya.lyugaeva@bk.ru>
2021-11-05 14:28:25 +0300

    refac use auth

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ const { login, createUser, logout } = require('../controllers/users');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const { validateLogin, validateUser } = require('../middlewares/validator');
+const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
 const router = Router();
 
 router.post('/signin', validateLogin, login);
 router.post('/signup', validateUser, createUser);
+
+router.use(auth);
 
 router.post('/signout', logout);
 
